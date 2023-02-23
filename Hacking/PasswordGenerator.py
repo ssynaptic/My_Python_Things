5482092a487fa00f4d2d50b559a79cea225d4bc0
2XTheTap <juanchislozano19@gmail.com>
2023-02-23 12:03:28 -0500

    Small changes on the Password Genrator

--- Hacking/PasswordGenerator.py
+++ Hacking/PasswordGenerator.py
@@ -1,35 +1,28 @@
-from optparse import OptionParser
+from argparse import ArgumentParser
 from random import choice
-from sys import exit
+from time import sleep
 import string
-def generate_password(length=15):
+def generate_password(length):
     lower_case = list(string.ascii_lowercase)
     upper_case = list(string.ascii_uppercase)
     simbols = ["!", "#", "$", "%", "&", "/", "(", ")", "=", "?",
                 "¿", "¡", "+", "*", "{", "}", "[", "]", ":", ".",
                 "-", "_"]
     characters = lower_case + upper_case + simbols
-    password = "".join([str(choice(characters)) for x in range(0, length)])
+    password = ("").join([str(choice(characters)) for x in range(0, length)])
     print(password)
 
 def get_arguments():
-    parser = OptionParser()
-    parser.add_option("--length", dest="length",
-                      default=15,
-                      help="The Lenght of the Password",
-                      metavar="LENGTH")
-    (options, arguments) = parser.parse_args()
-    return options
+    parser = ArgumentParser()
+    parser.add_argument("-l", "--length", dest="length",
+                        type=int, default=15,
+                        required=False,
+                        help="The Length of the password")
+    args = parser.parse_args()
+    return args
     
 if __name__ == "__main__":
-    try:
-        options = get_arguments()
-        if options.length == 15:
-            print("""Se generara la contraseña con una longitud por
-defecto de 15 caracteres""")
-            password = generate_password(int(options.length))
-        else:
-            password = generate_password(int(options.length))
-    except ValueError:
-        print("""\nDebe especificar la longitud de la contraseña
-con un numero entero""")
+        arguments = get_arguments()
+        print("The new password is below\n\r")
+        generate_password(arguments.length)
+        sleep(60)
