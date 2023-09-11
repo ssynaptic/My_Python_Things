f2d702c41c8a77fe4446cb24a438569b7aa2d36e
ssynaptic <juanchislozano19@gmail.com>
2023-09-11 08:11:26 -0500

    Finished the functionality to add records, working on the function to delete records

--- Others/pm/database.py
+++ Others/pm/database.py
@@ -2,72 +2,82 @@ from os.path import (exists,
                      isdir,
                      join,
                      dirname)
-from sqlite3 import connect
-# from signal import (signal,
-#                     SIGINT)
+from sqlite3 import (connect,
+                     OperationalError)
+from time import sleep
 
 class Database:
     def __init__(self):
         pass
     def check_db_name(self, db_name):
-        self.db_path = join(dirname(__file__), f"{db_name}.sqlite3")
-        self.db_is_valid = False
-        if exists(self.db_path) and isdir(self.db_path):
-            self.db_is_valid = "is_valid"
-        if exists(self.db_path) and not isdir(self.db_path):
-            self.db_is_valid = "there_is_an_equal"
-        if not exists(self.db_path) and not isdir(self.db_path):
-            self.db_is_valid = "is_valid"
+        db_path = join(dirname(__file__), db_name)
+        db_is_valid = False
+        if exists(db_path) and isdir(db_path):
+            db_is_valid = "is_invalid"
+        if exists(db_path) and not isdir(db_path):
+            db_is_valid = "there_is_an_equal"
+        if not exists(db_path) and not isdir(db_path):
+            db_is_valid = "is_valid"
 
-        return self.db_is_valid
-#        if exists(self.db_path):
-#            self.db_is_valid = False
-#            if isdir(self.db_path):
-#                self.db_is_valid = True
-#            else:
-#                self.db_is_valid = False
-#                return self.db_is_valid
-#        else:
-#            self.db_is_valid = True
-#            return self.db_is_valid
+        return db_is_valid
 
     def create_database(self, db_name):
-        self.conn = connect(database=db_name + ".sqlite3",
-                            timeout=1)
-        self.cursor = self.conn.cursor()
-        self.conn.commit()
-        self.conn.close()
+            conn = connect(database=db_name,
+                           timeout=1)
+            conn.commit()
+            conn.close()
+
+    def delete_database(self, db_name):
+        db_path = join(dirname(__file__), db_name)
+        if exists(db_path) and isdir(db_path):
+            remove(db_path)
 
     def create_main_table(self, db_name):
-        self.conn = connect(database=db_name + ".sqlite3",
-                            timeout=1)
-        self.cursor = self.conn.cursor()
-        self.table_instruction = """CREATE TABLE 'passwords'
+        conn = connect(database=db_name,
+                       timeout=1)
+        cursor = conn.cursor()
+        table_instruction = """CREATE TABLE 'passwords'
 (id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT NOT NULL,
 password TEXT NOT NULL);"""
-        self.cursor.execute(self.table_instruction)
-        self.conn.commit()
-        self.conn.close()
+        cursor.execute(table_instruction)
+        conn.commit()
+        conn.close()
 
     def create_record(self, db_name, username, password):
-        self.conn = connect(database=db_name + ".sqlite3",
-                            timeout=1)
-        self.cursor = self.conn.cursor()
-        self.record_instruction = """INSERT INTO passwords (username, password)
-VALUES (\"{username}\", \"{password}\")"""
-        self.cursor.execute(self.record_instruction)
-        self.conn.commit()
-        self.conn.close()
+        conn = connect(database=db_name,
+                       timeout=1)
+        cursor = conn.cursor()
+        record_instruction = f"""INSERT INTO passwords (username, password)
+VALUES ('{username}', '{password}')"""
+        cursor.execute(record_instruction)
+        conn.commit()
+        conn.close()
+
+    def delete_record(self, db_name):
+        pass
+
+    def get_data_from_db(self, db_name):
+        conn = connect(database=db_name,
+                       timeout=1)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM passwords;")
+        data = cursor.fetchall()
+        return data
 
     def check_db_integrity(self, db_name):
-        self.conn = connect(database=db_name + ".sqlite3",
-                            timeout=1)
-        self.cursor = self.conn.cursor()
-        self.checking_instruction = """SELECT NAME FROM
+        #self.conn = connect(database=db_name + ".sqlite3",
+        #                    timeout=1)
+        conn = connect(database=db_name,
+                       timeout=1)
+        cursor = conn.cursor()
+        checking_instruction = """SELECT NAME FROM
 sqlite_schema WHERE type = \"table\" AND name NOT LIKE
 \"sqlite_%\";"""
-        self.checking = self.cursor.execute(self.checking_instruction)
+        checking = cursor.execute(checking_instruction).fetchall()
         #if self.checking_results:
-        if len(self.checking) = 1 and len(self.checking)
-        return self.checking_results.fetchall()
+        if len(checking) == 1 and len(checking[0]) == 1 and checking[0][0] == "passwords":
+            # return self.checking_results.fetchall()
+            return "is_valid"
+        else:
+            return "is_invalid"
--- Others/pm/main.py
+++ Others/pm/main.py
@@ -5,6 +5,8 @@ from time import sleep
 from signal import (signal,
                     SIGINT,
                     SIGTSTP)
+from shutil import get_terminal_size as ts
+import sys
 class App:
     def __init__(self):
         # print(signal.valid_signals())
@@ -14,29 +16,63 @@ class App:
         self.main()
     def main(self):
         self.database = Database()
-        self.check_db_results = self.database.check_db_name(db_name=self.args.db_name[0])
-        if self.check_db_results == "is_valid":
+        check_db_results = self.database.check_db_name(db_name=self.args.db_name[0])
+        if check_db_results == "is_valid":
             print("Welcome to the password manager")
             self.database.create_database(db_name=self.args.db_name[0])
             self.database.create_main_table(db_name=self.args.db_name[0])
             print("[+] Passwords Database Created Successfully", end="\n\n")
             # self.user_option = input("[?] Do you want to create a record? [Y/N] -> ").upper()
             while True:
-                self.user_option = int(input("""1- Create Record
+                try:
+                    user_option = int(input("""1- Create Record
 2- Delete Record
 3- Read Records\n"""))
-                if self.user_option == 1:
-                    self.get_record()
+                    if user_option == 1:
+                        self.get_record()
+                        break
+                    if user_option == 2:
+                        pass
+                    else:
+                        break
+                except ValueError:
+                    print("[!] The option must be a number not letter or punctuation symbol")
                     break
-                else:
+                except Exception as _:
+                    print("[!] An unexpected error has ocurred")
                     break
             exit(code=0)
-        if self.check_db_results == "there_is_an_equal":
+        if check_db_results == "there_is_an_equal":
             # print("[-] That is not a valid name, enter another")
             print("[!] There is an existing database with the same name, it will be opened...")
-            print(self.args.db_name[0])
-            self.cresults = self.database.check_db_integrity(db_name=self.args.db_name[0])
-            print(self.cresults)
+            self.checking_table_results = self.database.check_db_integrity(db_name=self.args.db_name[0])
+            if self.checking_table_results == "is_valid":
+                print("[+] The content of the database is not altered")
+                while True:
+                    try:
+                        user_option = int(input("""1- Create Record
+2- Delete Record
+3- Read Records\n"""))
+                        if user_option == 1:
+                            self.get_record()
+                        if user_option == 2:
+                            self.delete_record()
+                    except ValueError:
+                        print("[!] The option must be a number not letter or punctuation symbol")
+                        break
+                    except Exception as _:
+                        print("[!] An unexpected error has ocurred")
+                        print(_)
+                        break
+            else:
+                print("[-] The content of the database is altered")
+                answer = input("Do you want to delete that database? [Y/N]: ").upper()
+                #if answer == "Y":
+
+            exit(code=0)
+
+        if check_db_results == "is_invalid":
+            print("[-] There seems to be a file or folder with the same name, so you cannot create the database with that name")
             exit(code=1)
 
     def get_record(self):
@@ -57,6 +93,13 @@ class App:
         else:
             print("[-] Bad Input")
             exit(code=1)
+
+    def delete_record(self):
+        data = self.database.get_data_from_db(self.args.db_name[0])
+        columns, rows = ts().columns, ts().lines
+        print(" ", (columns-2) * "_", sep="")
+        print(" ", "|", (columns-4) * " ", "|", sep="")
+        #print(columns, rows, sep="+")
     def valid_user_input(self, input_data):
         for char in input_data:
             if char in punctuation:
@@ -80,6 +123,6 @@ class App:
     def signal_handler(self, signum, frame):
         print("Exiting...")
         sleep(1)
-        exit(code=1)
+        exit(code=0)
 if __name__ == "__main__":
     app = App()
