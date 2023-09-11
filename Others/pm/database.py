e860a9027c3e84bcb112715b5e51d9a46fbbc471
ssynaptic <juanchislozano19@gmail.com>
2023-09-10 19:38:26 -0500

    Advances in the password manager: Added the function to create records, more functionalities coming soon

--- Others/pm/database.py
+++ Others/pm/database.py
@@ -3,6 +3,8 @@ from os.path import (exists,
                      join,
                      dirname)
 from sqlite3 import connect
+# from signal import (signal,
+#                     SIGINT)
 
 class Database:
     def __init__(self):
@@ -10,19 +12,62 @@ class Database:
     def check_db_name(self, db_name):
         self.db_path = join(dirname(__file__), f"{db_name}.sqlite3")
         self.db_is_valid = False
-        if exists(self.db_path):
-            self.db_is_valid = False
-            if isdir(self.db_path):
-                self.db_is_valid = True
-            else:
-                self.db_is_valid = False
-                return False
-        else:
-            return True
-    
+        if exists(self.db_path) and isdir(self.db_path):
+            self.db_is_valid = "is_valid"
+        if exists(self.db_path) and not isdir(self.db_path):
+            self.db_is_valid = "there_is_an_equal"
+        if not exists(self.db_path) and not isdir(self.db_path):
+            self.db_is_valid = "is_valid"
+
+        return self.db_is_valid
+#        if exists(self.db_path):
+#            self.db_is_valid = False
+#            if isdir(self.db_path):
+#                self.db_is_valid = True
+#            else:
+#                self.db_is_valid = False
+#                return self.db_is_valid
+#        else:
+#            self.db_is_valid = True
+#            return self.db_is_valid
+
     def create_database(self, db_name):
-        self.conn = connect(database=f"{db_name}.sqlite3",
-                            timeout=5)
+        self.conn = connect(database=db_name + ".sqlite3",
+                            timeout=1)
         self.cursor = self.conn.cursor()
         self.conn.commit()
         self.conn.close()
+
+    def create_main_table(self, db_name):
+        self.conn = connect(database=db_name + ".sqlite3",
+                            timeout=1)
+        self.cursor = self.conn.cursor()
+        self.table_instruction = """CREATE TABLE 'passwords'
+(id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+password TEXT NOT NULL);"""
+        self.cursor.execute(self.table_instruction)
+        self.conn.commit()
+        self.conn.close()
+
+    def create_record(self, db_name, username, password):
+        self.conn = connect(database=db_name + ".sqlite3",
+                            timeout=1)
+        self.cursor = self.conn.cursor()
+        self.record_instruction = """INSERT INTO passwords (username, password)
+VALUES (\"{username}\", \"{password}\")"""
+        self.cursor.execute(self.record_instruction)
+        self.conn.commit()
+        self.conn.close()
+
+    def check_db_integrity(self, db_name):
+        self.conn = connect(database=db_name + ".sqlite3",
+                            timeout=1)
+        self.cursor = self.conn.cursor()
+        self.checking_instruction = """SELECT NAME FROM
+sqlite_schema WHERE type = \"table\" AND name NOT LIKE
+\"sqlite_%\";"""
+        self.checking = self.cursor.execute(self.checking_instruction)
+        #if self.checking_results:
+        if len(self.checking) = 1 and len(self.checking)
+        return self.checking_results.fetchall()
--- Others/pm/main.py
+++ Others/pm/main.py
@@ -1,20 +1,68 @@
 from argparse import ArgumentParser
 from database import Database
+from string import punctuation
+from time import sleep
+from signal import (signal,
+                    SIGINT,
+                    SIGTSTP)
 class App:
-    def __init__(self): 
+    def __init__(self):
+        # print(signal.valid_signals())
+        signal(SIGINT, self.signal_handler)
+        signal(SIGTSTP, self.signal_handler)
         self.get_arguments()
         self.main()
     def main(self):
-        self.databse = Database()
-        self.check_db_results = self.databse.check_db_name(self.args.db_name[0])
-        if self.check_db_results:
+        self.database = Database()
+        self.check_db_results = self.database.check_db_name(db_name=self.args.db_name[0])
+        if self.check_db_results == "is_valid":
             print("Welcome to the password manager")
-            self.databse.create_database(self.args.db_name[0])
+            self.database.create_database(db_name=self.args.db_name[0])
+            self.database.create_main_table(db_name=self.args.db_name[0])
+            print("[+] Passwords Database Created Successfully", end="\n\n")
+            # self.user_option = input("[?] Do you want to create a record? [Y/N] -> ").upper()
+            while True:
+                self.user_option = int(input("""1- Create Record
+2- Delete Record
+3- Read Records\n"""))
+                if self.user_option == 1:
+                    self.get_record()
+                    break
+                else:
+                    break
             exit(code=0)
-        else:
-            print("That is not a valid name, enter another")
+        if self.check_db_results == "there_is_an_equal":
+            # print("[-] That is not a valid name, enter another")
+            print("[!] There is an existing database with the same name, it will be opened...")
+            print(self.args.db_name[0])
+            self.cresults = self.database.check_db_integrity(db_name=self.args.db_name[0])
+            print(self.cresults)
             exit(code=1)
 
+    def get_record(self):
+        self.user_username = str(input("Please enter your registry username: "))
+        self.user_password = str(input("Please enter your registry password: "))
+        print("[+] Validating...")
+        sleep(1)
+        self.results = tuple(map(self.valid_user_input, (self.user_username, self.user_password)))
+
+        if self.results[0] == True and self.results[1] == True:
+            print("[+] Success")
+            print("[+] Creating Record...")
+            self.database.create_record(db_name=self.args.db_name[0],
+                                        username=self.user_username,
+                                        password=self.user_password)
+            sleep(1)
+            print("[+] Success")
+        else:
+            print("[-] Bad Input")
+            exit(code=1)
+    def valid_user_input(self, input_data):
+        for char in input_data:
+            if char in punctuation:
+                return False
+        else:
+            return True
     def get_arguments(self):
         self.parser = ArgumentParser()
         self.parser.add_argument("-n",
@@ -28,5 +76,10 @@ class App:
                                  metavar="<DB_NAME>",
                                  dest="db_name")
         self.args = self.parser.parse_args()
+
+    def signal_handler(self, signum, frame):
+        print("Exiting...")
+        sleep(1)
+        exit(code=1)
 if __name__ == "__main__":
     app = App()
